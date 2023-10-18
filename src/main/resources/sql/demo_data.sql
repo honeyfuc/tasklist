816afcdb3a26e946ec850ce19a6ce37876090648
Yaroslav <76592052+honeyfuc@users.noreply.github.com>
2023-10-18 19:01:07 +0300

    Fixed bugs with not creating demo data

--- src/main/resources/sql/demo_data.sql
+++ src/main/resources/sql/demo_data.sql
@@ -8,11 +8,11 @@ VALUES ('Buy cheese', null, 'TODO', '2023-01-29 12:00:00'),
        ('Clean rooms', null, 'DONE', null),
        ('Call Mike', 'Ask about meeting', 'TODO', '2023-02-01 00:00:00');
 
-INSERT INTO users_tasks (task_id, user_id)
-VALUES (1, 2),
+INSERT INTO users_tasks (user_id, task_id)
+VALUES (2, 1),
        (2, 2),
-       (3, 2),
-       (4, 1);
+       (2, 3),
+       (1, 4);
 
 INSERT INTO users_roles (user_id, role)
 VALUES (1, 'ROLE_ADMIN'),
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -31,5 +31,5 @@ CREATE TABLE IF NOT EXISTS users_roles
     user_id BIGINT NOT NULL,
     role VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id, role),
-    CONSTRAINT fk_users_roles_users FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION
+    CONSTRAINT fk_users_roles_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE NO ACTION
 );
