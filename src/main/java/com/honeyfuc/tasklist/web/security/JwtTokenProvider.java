5b7575bb536c3ea09fcc357545f565adc05e37e3
Yaroslav <76592052+honeyfuc@users.noreply.github.com>
2023-10-28 18:28:46 +0300

    Added mybatis realization for working with database.

--- src/main/java/com/honeyfuc/tasklist/web/security/JwtTokenProvider.java
+++ src/main/java/com/honeyfuc/tasklist/web/security/JwtTokenProvider.java
@@ -7,7 +7,6 @@ import com.honeyfuc.tasklist.service.UserService;
 import com.honeyfuc.tasklist.service.props.JwtProperties;
 import com.honeyfuc.tasklist.web.dto.auth.JwtResponse;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ClaimsBuilder;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
